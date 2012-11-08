6f5f982635bfbf55785b0445c64285708a90ae2e
Burak Emir <bqe@google.com>
2012-11-08 22:50:00 +0100

    fix primitive "bk", it did not evaluate its argument

--- src/Interpreter.dart
+++ src/Interpreter.dart
@@ -115,6 +115,7 @@ class Interpreter {
         break;
         
       case Primitive.BACK:
+        nodes = evalInScope(nodes, scope);
         NumberNode wn = nodes.head;
         nodes = nodes.tail;
         turtle.back(wn.getNumValue());
