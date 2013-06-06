de1353c9d562f18e5f479253f566af778df0770a
Burak Emir <burak.emir@gmail.com>
2013-06-06 07:27:06 +0200

    simplify deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 rm -Rf deploy/*
 mkdir -p deploy/web
+mkdir -p deploy/web/packages/browser
+cp packages/browser/dart.js deploy/web/packages/browser/dart.js
 cp web/*.css web/*.dart web/*.js deploy/web/
-cp web/ArrowLogoDeploy.html deploy/web/ArrowLogo.html
+#cp web/ArrowLogoDeploy.html deploy/web/ArrowLogo.html
+cp web/ArrowLogo.html deploy/web/ArrowLogo.html
 cp web/ArrowLogoRedirect.html deploy/ArrowLogo.html
 cp -r jslib deploy/
+# for easy testing
+cp util/miniwebserver.dart deploy/
 
