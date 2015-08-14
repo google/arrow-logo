8224982a5ddfd3d4f8eb37059d4e9b645bcc84b7
Buraq <burak.emir@gmail.com>
2015-08-13 22:30:45 -0700

    fix bootstrap

--- web/arrowlogo.dart
+++ web/arrowlogo.dart
@@ -13,7 +13,7 @@
 // limitations under the License.
 library arrowlogo;
 
-import 'package:angular2/angular2.dart';
+import 'package:angular2/bootstrap.dart';
 import 'package:angular2/src/reflection/reflection.dart' show reflector;
 import 'package:angular2/src/reflection/reflection_capabilities.dart' show ReflectionCapabilities;
 import 'package:arrowlogo/arrow_logo_app.dart';
